f0b9bfe1f60f334075ac130d26f2b471d6d0e1a7
HyeLim Park <pinguino.lim@gmail.com>
2018-08-13 11:50:30 +0200

    Delete DeepMLP_MNIST.py

--- data/DeepMLP_MNIST.py
+++
@@ -1,5 +0,0 @@
-#import tensorflow as tf
-from keras.datasets import mnist  # subroutines for fetching the MNIST dataset
-from keras.models import Model  # basic class for specifying and training a neural network
-from keras.layers import Input, Dense  # the two types of neural network layer we will be using
-from keras.utils import np_utils  # utilities for one-hot encoding of ground truth values
